807eddfce2f1d7551137627b51dafea38d5a7d6e
Randeep Singh <singhr7347@gmail.com>
2024-12-21 09:54:58 +0530

    Initial commit

---
+++ datatypes.js
@@ -0,0 +1,29 @@
+
+console.log(3+3);
+console.log("Hitesh ");
+// Code readiblity should be high 
+
+let name ="Randeep";
+let age= 25;
+let isLoggedIn= false
+
+// number => 2 to power 53
+// bigint 
+// String => prefer to use in double codes =>""
+// boolean => true/false
+// null => standalone value
+// undefined => means currently it doesn't assigned  any values 
+//symbol => mosty it used for uniqness
+
+// object
+
+console.log(typeof age );
+// its type is number 
+console.log(typeof "Hitesh");
+// its type is String 
+
+console.log(typeof null);
+// Its type is object
+
+console.log(typeof undefined);
+// Its type is undefined
